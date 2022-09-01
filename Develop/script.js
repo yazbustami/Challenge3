09dd5b3d2d64b6f8b0faed1847243f60f1420b24
Yasmin Bustami <yasminbustami@Yasmins-MacBook-Pro.local>
2022-09-01 18:17:10 -0500

    Password length and selection validations complete

--- Develop/script.js
+++ Develop/script.js
@@ -8,54 +8,68 @@ const lc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"
 const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const spc = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "+", "="]
 
-let passArray = [];
-let pass = []
 
 //function to make genpass a function with inputting value heading. 8-128
 function generatePassword() {
-  let length = prompt ("How many characters should the password have?");
+  let length = prompt("How many characters should the password have?");
+  let passArray = [];
+  let pass = []
 
-  if (length < 8 || length > 128)
+  if (length < 8 || length > 128 || isNaN(length)) {
+  return "Please enter correct numerical value"
+  }
+
+
+  //types of characters
+  let upc = confirm("Would you like to have Uppercase?");
+  let lc = confirm("Would you like to have lowercase?");
+  let num = confirm("Would you like to have numbers?");
+  let spc = confirm("Would you like to have special characters?");
+
+  if (!upc && !lc && !num & !spc) {
+    alert("Please select at least one type of character. Please try again.")
+    return "Please select one character type"
+  }
+
+
+  //push array to new array if true.
+  //concat=joining arrays
+  if (upc) {
+    passArray = passArray.concat(upc)
+  }
+  if (lc) {
+    passArray = passArray.concat(lc)
+  }
+  if (num) {
+    passArray = passArray.concat(num)
+  }
+  if (spc) {
+    passArray = passArray.concat(spc)
+  }
+
+  for (let index = 0; index < length; index++) {
+    pass.push(passArray[Math.floor(Math.random() * passArray.length)])
+  }
+
+  //join
+  pass = pass.join("");
+
+  //array for new pass
+  passArray = [];
+  return (pass)
 }
-
-//types of characters
-let upc = confirm ("Would you like to have Uppercase?");
-let lc = confirm ("Would you like to have lowercase?");
-let num = confirm ("Would you like to have numbers?");
-let spc = confirm ("Would you like to have special characters?");
-
-if (!upc && !lc && !num & !spc) {
-  alert ("Please select at least one type of character. Please try again.")
-  return ("Please select one character type")
-}
-
-
-//push array to new array if true.
-//concat=joining arrays
-if (upc){
-  passArray=passArray.concat(upc)
-}
-if (lc){
-  passArray=passArray.concat(lc)
-}
-if (num){
-  passArray=passArray.concat(num)
-}
-if (spc){
-  passArray=passArray.concat(spc)
-}
-
-
-
-
 
 // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let passwordDisplay = generatePassword();
+  let passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
+  passwordText.value = passwordDisplay;
+  pass = [];
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 3rd challenge of the class
 LiveLink: https://yazbustami.github.io/Challenge3/
 
-Steps I took into considertaion :
+few things I took into considertaion :
     figure out what *characters* are needed
     min & max # of characters
